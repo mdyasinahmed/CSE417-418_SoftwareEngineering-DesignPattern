f9ce1f1d001e9bfc08aab66e2a72dd92d38ba1cf
mdyasinahmed <yasinahmed7875@gmail.com>
2025-03-10 11:11:51 +0600

    add Game Strategy - Strategy Design Pattern lab task code

---
+++ Lab_07/GameStrategy/GameStrategy_Main.java
@@ -0,0 +1,65 @@
+package Lab_07.GameStrategy;
+
+interface AttackStrategy{
+    public void performAttack();
+}
+
+class Melee_Attack implements AttackStrategy{
+    public void performAttack(){
+        System.out.println("Stabs with a knife for a melee strike!");
+    }
+}
+
+class Ranged_Attack implements AttackStrategy{
+    public void performAttack(){
+        System.out.println("Fires with Riffle from a distance!");
+    }
+}
+class Magic_Attack implements AttackStrategy{
+    public void performAttack(){
+        System.out.println("Casts a fireball with magic!");
+    }
+}
+
+class StealthAttack implements AttackStrategy{
+    public void performAttack(){
+        System.out.println("Attacked with Stealth!");
+    }
+}
+
+class Context {
+    private AttackStrategy attack_strategy;
+
+    public Context(AttackStrategy attack_strategy) {
+        this.attack_strategy = attack_strategy;
+    }
+
+    public void doAttack() {
+        attack_strategy.performAttack();
+    }
+    public void new_line(){
+        System.out.println();
+    }
+}
+
+
+
+public class GameStrategy_Main {
+    public static void main(String args[]){
+        Context context = new Context(new Melee_Attack());
+        context.doAttack();
+        context.new_line();
+
+        context = new Context(new Ranged_Attack());
+        context.doAttack();
+        context.new_line();
+
+        context = new Context(new Magic_Attack());
+        context.doAttack();
+        context.new_line();
+
+        context = new Context(new StealthAttack());
+        context.doAttack();
+        context.new_line();
+    }
+}
