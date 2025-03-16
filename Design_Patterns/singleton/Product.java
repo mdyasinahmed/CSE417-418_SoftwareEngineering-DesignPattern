dbcf3922ca67859d4c0e6c19b07f83cc122d1aba
mdyasinahmed <yasinahmed7875@gmail.com>
2025-03-17 00:04:19 +0600

    add Singleton design pattern practice code

---
+++ Design_Patterns/singleton/Product.java
@@ -0,0 +1,24 @@
+package Design_Patterns.singleton;
+
+public class Product {
+    private static Product product_instance;
+    private static int product_counts = 0;
+    
+        private Product(){
+            System.out.println("Product Object Initialized");
+        }
+    
+        public static Product getProduct(){
+            if (product_counts == 0) {
+                product_instance = new Product();
+            }
+            product_counts++;
+
+            return product_instance;
+        }
+
+        public void printUpdate(){
+            System.out.println("Product " + product_counts + " Created!");
+            System.out.println("Total Proudcts: " + product_counts + "\n");
+        }
+}
